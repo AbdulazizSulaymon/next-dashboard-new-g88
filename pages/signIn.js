40811363bcc51623b0d378cf2f86c5607ebd7aee
Abdulloh <pb6210770@gmail.com>
2021-08-23 14:36:17 +0500

    qwe

--- pages/signIn.js
+++ pages/signIn.js
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import SignInWrapper from "../Wrappers/SignInWrapper";
 
@@ -5,7 +6,22 @@ const SignIn = () => {
   return (
     <SignInWrapper>
       <div className="container">
-        <h1>Sign In</h1>
+        <div className="d-flex justify-content-between">
+          <p className="fw-bold">Soft UI Dashboard</p>
+          <ul className="d-flex ">
+            <li>Dashboard</li>
+            <li>Profile</li>
+            <li>Sign Up</li>
+            <li>Sign In</li>
+          </ul>
+          <Button variant="contained" className="rounded-pill px-5">
+            FREE DOWNLOAD
+          </Button>
+        </div>
+        <h3>Welcome back</h3>
+        <p>
+          Enter your email and password to sign <br></br>in
+        </p>
       </div>
     </SignInWrapper>
   );
