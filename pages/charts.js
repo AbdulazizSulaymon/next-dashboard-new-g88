b445db5542990b024dced947bba5a95be346dbf8
IslomTozaboyev <itozaboyev@gmail.com>
2021-08-30 10:01:02 +0500

    chart

--- pages/charts.js
+++ pages/charts.js
@@ -1,53 +1,87 @@
-import React from 'react'
-import { Bar } from 'react-chartjs-2';
-import Layout from '../containers/Layout';
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import Layout from "../containers/Layout";
 
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [
-        {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 1,
-        },
-    ],
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
 };
 
 const options = {
-    scales: {
-        yAxes: [
-            {
-                ticks: {
-                    beginAtZero: true,
-                },
-            },
-        ],
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
+};
+
+const data2 = {
+  labels: ["1", "2", "3", "4", "5", "6"],
+  datasets: [
+    {
+      label: "# of Red Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: "rgb(255, 99, 132)",
     },
+    {
+      label: "# of Blue Votes",
+      data: [2, 3, 20, 5, 1, 4],
+      backgroundColor: "rgb(54, 162, 235)",
+    },
+    {
+      label: "# of Green Votes",
+      data: [3, 10, 13, 15, 22, 30],
+      backgroundColor: "rgb(75, 192, 192)",
+    },
+  ],
+};
+
+const options2 = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: true,
+        },
+      },
+    ],
+  },
 };
 
 const Charts = () => {
-    return (
-        <Layout>
-            <h1>Bar</h1>
-            <Bar data={data} options={options} />
-        </Layout>
-    )
-}
+  return (
+    <Layout>
+      <h1>Bar</h1>
+      <Bar data={data2} options={options2} />
+      <Bar data={data} options={options} />
+    </Layout>
+  );
+};
 
-export default Charts
+export default Charts;
